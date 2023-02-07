319b1f5780b3e26b1cd8ebd4716370f0593143f8
jason-xy <1462357003@qq.com>
2023-02-08 00:02:40 +0800

    support realsense cam in docker container

--- docker/run.sh
+++ docker/run.sh
@@ -1,6 +1,10 @@
+#!/bin/bash
 sudo docker run --runtime nvidia -it --rm \
+		--name vins \
+		--privileged \
 		--network host \
 		-e DISPLAY=$DISPLAY \
+		-v /dev:/dev \
 		-v /tmp/.X11-unix/:/tmp/.X11-unix \
 		-v /tmp/argus_socket:/tmp/argus_socket \
 		-v /etc/enctune.conf:/etc/enctune.conf \
