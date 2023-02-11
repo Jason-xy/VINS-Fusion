c4bed6c44b0e0ff1503b0c1c2b7657dab6e5dab4
Jason_xy <53471811+Jason-xy@users.noreply.github.com>
2023-02-11 20:56:56 +0800

    one click to run vins demo (#3)
    * one click to run vins demo on amd64
    * one click to run vins demo on arm64
    * launch vins cross node

--- docker/amd64/run-amd64.sh
+++ docker/amd64/run-amd64.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-docker run --gpus all -it --rm \
+script_dir=$(cd $(dirname $0);pwd)
+docker run --gpus all -it \
 		--name vins \
 		--privileged \
 		--network host \
 		-e DISPLAY=$DISPLAY \
-		-v /dev:/dev \
 		-v /tmp/.X11-unix/:/tmp/.X11-unix \
-		-v /tmp/argus_socket:/tmp/argus_socket \
-		-v /etc/enctune.conf:/etc/enctune.conf \
+		-v $script_dir/../../:/root/ros2_ws/src/ \
         jasonxxxyyy/sky-explorer:runtime-cuda11.4-ros2-amd64 \
-        /bin/bash
+       	/root/ros2_ws/src/docker/scripts/vins_demo.py --rviz
