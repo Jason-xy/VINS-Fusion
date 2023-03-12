a8cf081f5822166f7da5811f5512c029030882ae
jason-xy <1462357003@qq.com>
2023-03-12 17:06:15 +0800

    cross compile for arm64

--- docker/scripts/compile.sh
+++ docker/scripts/compile.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
-script_dir=$(cd $(dirname $0);pwd)
-file=/usr/bin/qemu-aarch64-static
-if [ ! -f "$file" ]; then
-  wget https://github.com/multiarch/qemu-user-static/releases/download/v7.2.0-1/qemu-aarch64-static -o /usr/bin/qemu-aarch64-static
-fi
-
-docker run --rm \
-        --network host \
-        --privileged \
-        -v /usr/bin/qemu-aarch64-static:/usr/bin/qemu-aarch64-static \
-        -v $script_dir/../../:/root/ros2_ws/src/ \
-        jasonxxxyyy/sky-explorer:runtime-cuda11.4-ros2-arm64 \
-        /root/ros2_ws/src/docker/scripts/vins_demo.py --pack
+sudo docker run --rm \
+    -v `pwd`:/root/ros2_ws/src \
+    jasonxxxyyy/sky-explorer:runtime-cuda11.4-ros2-$1 \
+    /bin/bash -c "cd /root/ros2_ws && \
+            source /opt/ros/foxy/setup.bash && \
+            colcon build --symlink-install --allow-overriding cv_bridge --cmake-args -DCMAKE_BUILD_TYPE=Release"
