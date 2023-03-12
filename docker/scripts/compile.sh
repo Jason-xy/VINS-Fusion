526b5b5aa9b2ab2c292c41dcbf99cdcc7a07b238
jason-xy <1462357003@qq.com>
2023-03-12 15:03:50 +0800

    cross compile for arm64

--- docker/scripts/compile.sh
+++ docker/scripts/compile.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 script_dir=$(cd $(dirname $0);pwd)
-sudo docker run --rm --runtime nvidia -it \
-		--privileged \
+docker run --rm --privileged multiarch/qemu-user-static:register --reset
+docker run --rm -it \
 		--network host \
-		-e DISPLAY=$DISPLAY \
 		-v /usr/bin/qemu-aarch64-static:/usr/bin/qemu-aarch64-static \
-		-v /dev:/dev \
-		-v /tmp/.X11-unix/:/tmp/.X11-unix \
-		-v /tmp/argus_socket:/tmp/argus_socket \
-		-v /etc/enctune.conf:/etc/enctune.conf \
 		-v $script_dir/../../:/root/ros2_ws/src/ \
         jasonxxxyyy/sky-explorer:runtime-cuda11.4-ros2-arm64 \
         /root/ros2_ws/src/docker/scripts/vins_demo.py --pack
